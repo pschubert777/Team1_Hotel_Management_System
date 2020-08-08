87c7efdf15e8121ffb2d80de0ef19608748444e7
pschubert777 <44069136+pschubert777@users.noreply.github.com>
2020-08-08 11:40:14 -0400

    Update Transactions.sql

--- Hotel_Management_System/Hotel_Entity_Relationship_System/Transactions.sql
+++ Hotel_Management_System/Hotel_Entity_Relationship_System/Transactions.sql
@@ -7,6 +7,7 @@
     [Money_spent] FLOAT NOT NULL DEFAULT 0, 
     [Activity_type] NVARCHAR(MAX) NULL, 
     [Reward_points_gained] INT NOT NULL DEFAULT 0, 
+    [Transaction_date] DATE NOT NULL, 
     CONSTRAINT [Reservation_ID] FOREIGN KEY ([Reservation_Id]) REFERENCES [Reservation]([Id]), 
     CONSTRAINT [Customeer_ID] FOREIGN KEY ([Customer_Id]) REFERENCES [Customer]([Id]) 
 )
