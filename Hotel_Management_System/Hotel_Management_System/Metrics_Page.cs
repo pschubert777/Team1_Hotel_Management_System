2df9894ca473cb2f5d2df3e3bba06bb3827722ee
pschubert777 <44069136+pschubert777@users.noreply.github.com>
2020-08-13 17:43:23 -0400

    Update Metrics_Page.cs

--- Hotel_Management_System/Hotel_Management_System/Metrics_Page.cs
+++ Hotel_Management_System/Hotel_Management_System/Metrics_Page.cs
@@ -14,7 +14,7 @@ namespace Hotel_Management_System
 {
     public partial class Metrics_Page : Form
     {
-        private string user_id {get; set;}
+        private int user_id {get; set;}
         private string user_type { get; set; }
         public Metrics_Page()
         {
