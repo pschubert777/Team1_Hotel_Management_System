7c7ec1e84c4bc289ba1c56161c1136fb4c22acbd
pschubert777 <44069136+pschubert777@users.noreply.github.com>
2020-08-17 20:29:07 -0400

    maintenance

--- Hotel_Management_System/Hotel_Management_System/Maintenance.cs
+++ Hotel_Management_System/Hotel_Management_System/Maintenance.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Data.SqlClient;
 using System.Data;
+using System.Globalization;
 
 namespace Hotel_Management_System
 {
@@ -23,13 +24,33 @@ namespace Hotel_Management_System
                 using (StreamReader readFile = new StreamReader(@"C:\Users\peterschubert\Documents\FilesTest\Maintenance.txt"))
                 {
                     string maintenance_input = "";
-
+                    DateTime date = DateTime.Now.Date;
+                    int hotel_id = 0;
                     while ((maintenance_input = readFile.ReadLine()) != null)
                     {
+                        string[] maint_input = maintenance_input.Split(' ');
+                        
+                        if (string.Equals(maint_input[0], "D"))
+                        {
+                            continue;
+                        }
+                        else if(string.Equals(maint_input[0], "M"))
+                        {
+                            date =DateTime.ParseExact(maint_input[9], "yyyyMMdd", CultureInfo.InvariantCulture);
+                        }
+                        else if( string.Equals(maint_input[0], "H")){
 
+                            hotel_id = Convert.ToInt32(hotel_id);
+                        }
+                        else if(string.Equals (maint_input[0], "R"))
+                        {
+                            int room_id = search_room_id(Convert.ToInt32(maint_input[1]), hotel_id);
 
-
-
+                            if(!check_duplicates(room_id, date))
+                            {
+                                insert_into_maintenance(room_id, date);
+                            }
+                        }
 
                     }
                 }
@@ -75,6 +96,45 @@ namespace Hotel_Management_System
             return room_id;
         }
 
+        // check if a already schedule maintenance for the room on a given date exists so we dont enter it in twice
+        private bool check_duplicates(int room_id, DateTime date)
+        {
+            bool are_duplicates = false;
+            using (SqlConnection Connection = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=Hotel_Entity_Relationship_System;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                Connection.Open();
+
+                using (SqlCommand query = new SqlCommand("Select Count(*) from Maintenance Where Id=@RoomId and Date_maintenance =@DateMaintenance", Connection))
+                {
+                    int count = 0;
+                    query.Parameters.AddWithValue("@RoomId ", room_id);
+                    query.Parameters.AddWithValue("@DateMaintenance", date.Date);
+                    count = Convert.ToInt32(query.ExecuteScalar());
+                    if (count > 0){are_duplicates = true; }
+                    
+
+                }
+
+            }
+            return are_duplicates;
+        }
+        private void insert_into_maintenance(int room_id, DateTime date)
+        {
+            using (SqlConnection Connection = new SqlConnection(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=Hotel_Entity_Relationship_System;Integrated Security=True;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
+            {
+                Connection.Open();
+
+                using (SqlCommand query = new SqlCommand("Insert into Maintenance (Id, Date_maintenance) Values (@RoomId, @DateMaintenance)  ", Connection))
+                {
+                    query.Parameters.AddWithValue("@RoomId ", room_id);
+                    query.Parameters.AddWithValue("@DateMaintenance", date);
+                    query.ExecuteNonQuery();
+
+
+                }
+
+            }
+        }
 
 
     }
--- Hotel_Management_System/Hotel_Management_System/Program.cs
+++ Hotel_Management_System/Hotel_Management_System/Program.cs
@@ -32,6 +32,9 @@ namespace Hotel_Management_System
             //Application.Run(new reservation_page());
             //Application.Run(new Display_Logs());
             Application.Run(new frmLogin());
+
+            Maintenance clean = new Maintenance();
+            clean.read_file();
         }
     }
 }
